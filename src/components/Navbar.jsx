2b0cda7f7286a2bec8fa2f772cad941a2c176539
Kapil Kaswan <kaswankapil22@gmail.com>
2026-02-07 13:36:37 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import logo from "/src/assets/logo.jpeg"
+import logo from "../assets/logo.jpeg";"
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
